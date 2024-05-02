45366598f893c098bafbc1e0f90be1121ab955b8
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-05-02 19:39:25 +0100

    basic if statement with variables

--- ExOneVariables.py
+++ ExOneVariables.py
@@ -1,5 +1,12 @@
 desired_age = 25
 real_age = 31
 name = 'Jake'
-print(name + " wants to be " + str(age) + " years old.")
 
+print(name + " wants to be " + str(desired_age) + " years old.")
+
+if desired_age < real_age:
+    print(name + " is " + str(real_age) + ". He wishes he was younger so he could learn Python earlier.")
+elif desired_age > real_age:
+    print(name + " is " + str(real_age) + ". He wishes he was older. He wants those cheaper cinema tickets.")
+else:
+    print(name + " is " + str(real_age) + ". He is exactly the age he wants to be. Go him!")
