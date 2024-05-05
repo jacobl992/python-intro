76b9ad274d8e7fbcd84a3aa841442ca198e35bc2
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-05-05 17:12:33 +0100

    string indexing

--- ExOneVariables.py
+++ ExOneVariables.py
@@ -4,9 +4,18 @@ name = 'Jake'
 
 print(name + " wants to be " + str(desired_age) + " years old.")
 
+#simple if statement
 if desired_age < real_age:
     print(name + " is " + str(real_age) + ". He wishes he was younger so he could learn Python earlier.")
 elif desired_age > real_age:
     print(name + " is " + str(real_age) + ". He wishes he was older. He wants those cheaper cinema tickets.")
 else:
     print(name + " is " + str(real_age) + ". He is exactly the age he wants to be. Go him!")
+
+#index a string
+print('What is the second letter in the name?')
+print(name[1])
+print('What is the last letter in the name?')
+print(name[-1])
+print('What are the middle two letters in the name?')
+print(name[1:3])
