2a0908a28bc690134648630b867f4bda32ee893f
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-05-05 17:01:16 +0100

    weight conversion

--- ExTwoInput.py
+++ ExTwoInput.py
@@ -1,4 +1,14 @@
+#name and fav colour
 name = input('What is your name? ')
 fav_colour = input('What is your favourite colour? ')
 print(name + ' likes ' + fav_colour)
 
+#birth year calculation
+birth_year = input('Birth year: ')
+age = 2024 - int(birth_year)
+print('Your age is ' + str(age))
+
+#weight converson
+weight_metric = input('What is your weight in kg? ')
+weight_imperial = round(float(weight_metric) * 2.2, 2)
+print('Your weight in pounds is ' + str(weight_imperial) + '.')
