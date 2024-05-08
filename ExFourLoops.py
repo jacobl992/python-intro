2b4b1a7350d718e63f6b118f3970ae98fcd7c4ca
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-05-08 17:03:44 +0100

    while loop guessing game

--- ExFourLoops.py
+++ ExFourLoops.py
@@ -4,3 +4,21 @@ while i <= 5:
     print(i * '*')
     i += 1
 print('done')
+
+#guessing game
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+correct_guess = False
+
+while guess_count < guess_limit and not correct_guess:
+    guess = int(input('Guess a number '))
+    guess_count += 1
+    if guess == secret_number:
+        print('Correct. You win')
+        correct_guess = True
+    elif guess_count == guess_limit:
+        print('Wrong, game over.')
+    else:
+        print('Wrong, try again')
+
