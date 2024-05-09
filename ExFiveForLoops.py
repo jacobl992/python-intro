13df9a70945838a2d31de38a02828e212987d1cd
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-05-09 11:39:12 +0100

    for loop sum example

--- ExFiveForLoops.py
+++ ExFiveForLoops.py
@@ -1,3 +1,11 @@
 #simple for loop
 for item in range(10):
     print(item)
+
+#shopping list example
+prices = [10, 12.5, 8, 2.2, 16, 3, 5, 1, 0.77, 7]
+total = 0
+
+for price in prices:
+    total += price
+print(f'Total: £{total}')
