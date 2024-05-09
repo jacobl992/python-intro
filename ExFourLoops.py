3ba534a31dfc2f902caea6cf7f3a96692a0b7e5e
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-05-09 11:24:14 +0100

    car game using while loop

--- ExFourLoops.py
+++ ExFourLoops.py
@@ -10,6 +10,7 @@ secret_number = 9
 guess_count = 0
 guess_limit = 3
 correct_guess = False
+    # could use 'break' statement instead
 
 while guess_count < guess_limit and not correct_guess:
     guess = int(input('Guess a number '))
@@ -22,3 +23,35 @@ while guess_count < guess_limit and not correct_guess:
     else:
         print('Wrong, try again')
 
+
+#car game
+command = ("")
+car_stopped = False
+car_started = False
+while True:
+    command = input("> ").lower()
+    if command == 'start':
+        if not car_started:
+            print('Car started... ')
+            car_started = True
+            car_stopped = False
+        else:
+            print('Car already running')
+    elif command == 'stop':
+        if not car_stopped:
+            print('Car stopped')
+            car_stopped = True
+            car_started = False
+        else:
+            print('Car already stopped.')
+    elif command == 'help':
+        print("""
+start - to start the car
+stop - to stop the car
+help - for instructions
+quit - to end game
+        """)
+    elif command == 'quit':
+        break
+    else:
+        "Sorry, i don't understand that"
