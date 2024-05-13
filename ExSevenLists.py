0e1ea7c48a8817d6633673062443baeeb6144cec
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-05-13 12:36:04 +0100

    list methods

--- ExSevenLists.py
+++ ExSevenLists.py
@@ -27,3 +27,25 @@ print(matrix[0][1])
 for row in matrix:
     for item in row:
         print(item)
+
+#list methods
+numbers.append(52)
+print(numbers)
+numbers.insert(0,10)
+    #insert new item at begining of list
+print(numbers)
+numbers.remove(10)
+print(numbers)
+numbers.pop()
+    #removes last number in list
+    #.clear clears entire list
+print(numbers.index(4))
+    #returns index of value in list
+print(52 in numbers)
+    #returns boolean value determining  if value in list
+numbers.sort()
+    #sort in ascending order
+    #.reverse would be in descending order
+numbers2 = numbers.copy()
+    #creates second table with copied data
+
