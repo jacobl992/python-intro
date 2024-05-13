31d2a174090d48e74f49aa30dba1852e0ed384ea
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-05-13 12:05:06 +0100

    nested loop Fshape challenge

--- ExSixNestedLoops.py
+++ ExSixNestedLoops.py
@@ -1,6 +1,14 @@
+#coordinates example
 for x in range(4):
     print(x)
     for y in range(3):
         print(f'({x}, {y})')
 
-#challenge F-shape
+#challenge F-shape without string multiplier
+numbers = [5, 2, 5, 2, 2]
+
+for number in numbers:
+    output = ''
+    for i in range(number):
+        output += 'x'
+    print(output)
