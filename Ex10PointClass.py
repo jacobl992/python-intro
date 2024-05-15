60621f2c7c8e21c2ccfab373d18ecf30cd632af6
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-05-15 14:16:09 +0100

    using constructor with the class

--- Ex10PointClass.py
+++ Ex10PointClass.py
@@ -1,4 +1,11 @@
 class Point:
+    def __init__(self, x, y):
+        #magic method constructor
+        self.x = x
+        self.y = y
+        #setting the x or y attribute to the x or y argument passed to this function
+
+
     def move(self):
         print("move")
 
@@ -7,13 +14,14 @@ class Point:
         print("draw")
 
 
-point1 = Point()
+point1 = Point(10, 20)
 point1.x = 10
-point1.y = 20
+print(point1.x)
+point1.x = 11
 print(point1.x)
 point1.draw()
 
-point2 = Point()
+point2 = Point(12, 50)
 point2.x = 1
 #point1 and point2 are both seperate instances of the Point class
 # They can draw on the same methods, but have different values
