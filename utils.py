68b22bb75652f4ff36f8956e8a07f8aad324fcbe
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-05-15 15:06:36 +0100

    comment to exaplain overwriting of Python function

--- utils.py
+++ utils.py
@@ -3,4 +3,5 @@ def find_max(numbers):
     for number in numbers:
         if number > max:
             max = number
+            #we are overwrting the built in Python function of 'max' which is bad practise, but just doing this to complete the exercise
     return max
