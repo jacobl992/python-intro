1c198834b4fb8564cd0e87200e11b7dffab7232e
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-05-15 16:54:41 +0100

    basic package example

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import utils
+from packageexample.packagefunction import package_function_greeting
+
 print("Hello World!")
 
 number_list = [10, 3, 5, 1, 34, 7, 14, 19, 25, 65, 31]
 
 print(utils.find_max(number_list))
+
+package_function_greeting()
---
+++ packageexample/packagefunction.py
@@ -0,0 +1,2 @@
+def package_function_greeting():
+    print('here is a functon from a package')
