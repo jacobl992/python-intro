fbe467ec5037c6d49f5943308fbcab2b8826aa99
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-05-15 13:43:56 +0100

    Zero division error exception

--- ExNineExceptions.py
+++ ExNineExceptions.py
@@ -1,7 +1,11 @@
 #using a try block
 try:
     age = int(input('Age: '))
+    income = 20000
+    risk = income / age
     print(age)
+except ZeroDivisionError:
+    print('Age cannot be zero')
 except ValueError:
     print('Invalid value')
 
